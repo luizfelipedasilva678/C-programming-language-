28e0f5686563afb11bf6f33b2525a324a9d51a26
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-05-23 15:39:13 -0300

    Add files via upload

---
+++ C/comparaNumeros.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int n1, n2;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+    printf("\n");
+
+
+    if(n1 == n2)
+    {
+        printf("Sao iguais");
+    }
+
+    if(n1 > n2)
+    {
+        printf("Numero 1 e maior que numero 2");
+    }
+
+    if(n2 > n1)
+    {
+        printf("Numero 2 e maior que numero 1");
+    }
+
+}
