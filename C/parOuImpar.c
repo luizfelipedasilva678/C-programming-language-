dac75ed814e4ffc9201a80daebff4d4cc075d917
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-05-23 15:26:05 -0300

    Add files via upload

---
+++ C/parOuImpar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    if(n % 2 == 0)
+    {
+        printf("Par");
+    }
+
+    if(n % 2 != 0)
+    {
+        printf("Impar");
+    }
+}
