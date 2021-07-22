6295a6778f4d12490a4314bd2ddc1ea84e4fc405
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-07-22 18:57:23 -0300

    Add files via upload

---
+++ C/exercicio-1-3.c
@@ -0,0 +1,24 @@
+/*-----------
+ Luiz Felipe
+ Programa criado em 21/07/2021
+------------*/
+#include <stdio.h>
+#include <stdlib.h>void exibeNomeCompleto(char*, char*);
+main ()
+{
+ char *nome, *sobrenome;
+ nome = (char*)malloc(sizeof(char));
+ sobrenome = (char*)malloc(sizeof(char));
+ printf("Digite seu nome: ");
+ gets(nome);
+ printf("Digite seu sobrenome: ");
+ gets(sobrenome);
+ exibeNomeCompleto(nome, sobrenome);
+ free(nome);
+ free(sobrenome);
+}
+void exibeNomeCompleto(char *nome, char *sobrenome)
+{
+ printf("%s, %s", sobrenome, nome);
+}
+
